d88c52584ba50ca71fd1532219b1aefb7322a89a
Weellyyy <wildanprtm2304@gmail.com>
2024-04-02 20:41:53 +0700

    menambahkan int main

--- struktur4.cpp
+++ struktur4.cpp
@@ -13,3 +13,11 @@ struct mahasiswa {
     int umur;
 };
 
+int main(){
+    mahasiswa mhs[3];
+    for(int i=0;i<3;i++) {
+        cout <<"Data ke-"<<(i+1)<<":" <<endl;
+        cout << "nomor mahasiswa: ";
+        getline(cin,mhs[i].nim);
+        cout << "nama mahasiswa : ";
+        getline(cin,mhs[i].nama);
