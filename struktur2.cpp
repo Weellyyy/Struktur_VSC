195f3c3d32281178b773c88f5d844f53ed41a813
Weellyyy <wildanprtm2304@gmail.com>
2024-04-02 20:32:08 +0700

    penambahan prosedur struktur detail alamat dan mahasiswa

--- struktur2.cpp
+++ struktur2.cpp
@@ -5,3 +5,36 @@ struct DetailAlamat {
     string desa;
     string kota;
 };
+
+struct mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main(){
+    mahasiswa mhs;
+    cout << "nomor mahasiswa: ";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "alamat mahasiswa : "<<endl;
+    cout << "\t nama desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t nama kota : ";
+    cin >> mhs.alamat.kota;
+
+     cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout <<"\n nim : "<<mhs.nim;
+    cout <<"\n nama : "<<mhs.nama;
+    cout <<"\n alamat : ";
+    cout <<"\n \t desa : "<< mhs.alamat.desa;
+    cout <<"\n \t kota : "<< mhs.alamat.kota;
+
+}
+
